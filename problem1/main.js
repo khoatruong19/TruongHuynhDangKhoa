53993231998b22d0e8ec2e0de5d499843c947392
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2024-08-06 14:59:39 +0700

    feat: problem 1 solved

---
+++ problem1/main.js
@@ -0,0 +1,33 @@
+const sumToN = (n, calculation) => {
+    const isPositiveInteger = Number.isInteger(n) && n > 0;
+    if(!isPositiveInteger) return 0
+    
+    return calculation(n)
+}
+
+const sumByIterative = (n) => {
+    let sum = 0
+    for (let i = 1; i <= n; i++) {
+            sum += i
+    }
+    return sum
+};
+
+const sumByProgressionFormula = (n) => {
+    return (n*(n+1)) / 2
+};
+
+const sumByRecursive = (n) => {
+    if (n <= 1) {
+        return n;
+    }
+    return n + sumByRecursive(n - 1);
+};
+
+const resultA = sumToN(5, sumByIterative);
+const resultB = sumToN(5, sumByProgressionFormula);
+const resultC = sumToN(5, sumByRecursive);
+
+console.log(resultA); // Output: 15
+console.log(resultB); // Output: 15
+console.log(resultC); // Output: 15
